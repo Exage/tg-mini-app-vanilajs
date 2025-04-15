a8b9951773f06477ca4adc90cce3e81257aaa445
Nikita Horkavchuk <horkavchuk.nik@gmail.com>
2025-04-15 20:07:47 +0300

    init

---
+++ app.js
@@ -0,0 +1,5 @@
+const tg = window.Telegram.WebApp
+const user = tg.initDataUnsafe?.user
+
+document.getElementById('first_name').innerText = user?.first_name || "First Name"
+document.getElementById('username').innerText = user?.username || "@username"
