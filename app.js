09703865a2e3e029ee9b1bec6b959b162d562467
Nikita Horkavchuk <horkavchuk.nik@gmail.com>
2025-04-15 20:15:24 +0300

    small fix

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ const tg = window.Telegram.WebApp
 const user = tg.initDataUnsafe?.user
 
 document.getElementById('first_name').innerText = user?.first_name || "First Name"
-document.getElementById('username').innerText = user?.username || "@username"
+document.getElementById('username').innerText = `@${user?.username}` || "@username"
